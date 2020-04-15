1f7df880633437f455f1fd7b27eb482c055928f9
zhangatle <zhangatle@gmail.com>
2020-04-15 20:41:27 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@
 */
 
 Route::redirect('/','/products')->name('root');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
@@ -26,4 +24,8 @@ Route::group(['middleware' => ['auth','verified']],function (){
     Route::delete('user_addresses/{user_address}', 'UserAddressController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
