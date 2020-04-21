fea5999ac82cf8559ec1e518a5ddbc82c40ac872
zhangatle <zhangatle@gmail.com>
2020-04-21 18:11:26 +0800

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器注入一个名为alipay的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
+            $config['notify_url'] = 'http://requestbin.net/r/1268m071';
+            $config['return_url'] = route('payment.alipay.return');
             // 判断当前环境是否为线上环境
             if(app()->environment() != 'production') {
                 $config['mode'] = 'dev';
