ffc4434a48182fbf337a1ffcac08973d09a72f20
zhangatle <zhangatle@gmail.com>
2020-04-28 19:02:51 +0800

    订单模块调整

---
+++ app/Listeners/UpdateCrowdfundingProductProgress.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Models\Order;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+
+class UpdateCrowdfundingProductProgress implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPaid  $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        $order = $event->getOrder();
+        // 如果订单类型不是众筹订单，无需处理
+        if($order->type !== Order::TYPE_CROWDFUNDING) {
+            return;
+        }
+        $crowdfunding = $order->items[0]->product->crowdfunding;
+
+        $data = Order::query()
+            ->where('type',Order::TYPE_CROWDFUNDING)
+            ->whereNotNull('paid_at')
+            ->whereHas('items',function ($query) use ($crowdfunding) {
+                $query->where('product_id',$crowdfunding->product_id);
+            })
+            ->first([
+                DB::raw('sum(total_amount) as total_amount'),
+                DB::raw('count(distinct(user_id)) as user_count()'),
+            ]);
+        $crowdfunding->update([
+            'total_amount' => $data->total_amount,
+            'user_count' => $data->user_count,
+        ]);
+    }
+}
