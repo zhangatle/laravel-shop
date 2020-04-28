82f4759364fe3d3a40a6dc129dfa0e884806c895
zhangatle <319126583@qq.com>
2020-04-28 20:12:02 +0800

    分期付款配置

--- config/app.php
+++ config/app.php
@@ -229,4 +229,11 @@ return [
     ],
     'order_ttl' => 1800,
     'ngrok_url' => env('NGROK_URL'),
+    'installment_fee_rate' => [
+        3 => 1.5,
+        6 => 2,
+        12 => 2.5,
+    ],
+    'min_installment_amount' => 300,
+    'installment_fine_rate' => 0.05,
 ];
